a3abbd46d7ac80f3a73dea0ed2e92b650ec7378a
Dian Li <dianli@uchicago.edu>
2017-02-19 07:52:41 +0800

    Uploading missing files

---
+++ Prototypes/Assets/Script/DoorOpen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TouchScript.Gestures;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour {
+
+	private TapGesture tapGesture;
+	private void OnEnable()
+	{
+		GetComponent<TapGesture>().Tapped += tappedHandler;
+	}
+
+	private void OnDisable()
+	{
+		GetComponent<TapGesture>().Tapped -= tappedHandler;
+	}
+
+	private void tappedHandler(object sender, EventArgs e)
+	{
+		print("adfasdfasdfasdfasdf");
+	}
+}
