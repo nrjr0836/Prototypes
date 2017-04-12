9f7cdf721fda27bf7c1b05410542dde6554a825f
sxl <shenxia3@mail.utoronto.ca>
2017-04-12 00:11:01 -0700

    Animation and OnClick go back

---
+++ Prototypes/Assets/Script/Back.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Back : MonoBehaviour {
+
+
+	public GameObject DisableScene;
+	public GameObject EnableBack;
+
+
+	void OnMouseDown(){
+		DisableScene.SetActive (false);
+		EnableBack.SetActive (true);
+	}
+}
---
+++ Prototypes/Assets/Script/CollisionTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTest : MonoBehaviour {
+
+	public GameObject otherObject;
+	Animator otherAnimator;
+
+	// Use this for initialization
+	void Start () {
+		otherAnimator = otherObject.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		if(col.gameObject.tag == "Col")
+		{
+			otherAnimator.SetTrigger ("Personstand");
+			Destroy(gameObject);
+		}
+	}
+}
