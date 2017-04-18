68d4ddb025fc73a7ca4e992464cb266713ce3fe6
sxl <shenxia3@mail.utoronto.ca>
2017-04-18 14:23:28 -0700

    Fixed Animation State

--- Prototypes/Assets/Script/CollisionTest.cs
+++ Prototypes/Assets/Script/CollisionTest.cs
@@ -6,13 +6,10 @@ public class CollisionTest : MonoBehaviour {
 
 	public GameObject otherObject;
 	Animator otherAnimator;
-	public GameObject walkObject;
-	Animator walkAnimator;
 
 	// Use this for initialization
 	void Start () {
 		otherAnimator = otherObject.GetComponent<Animator> ();
-		walkAnimator = walkObject.GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -24,8 +21,7 @@ public class CollisionTest : MonoBehaviour {
 	{
 		if(col.gameObject.tag == "Col")
 		{
-			otherAnimator.SetTrigger ("Personstand");
-			walkAnimator.SetTrigger("Walk");
+			otherAnimator.SetBool ("Personstand", true);
 			Destroy(gameObject);
 		}
 	}
