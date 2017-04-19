3fd9e43b5db19ef2b2a489a215515e6fbabbf4f0
Dian Li <dian.mike.li@gmail.com>
2017-04-18 23:37:48 -0500

    Trying to solve gesture conflicts

---
+++ Prototypes/Assets/Script/AfterPressed.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using TouchScript.Gestures;
+using Random = UnityEngine.Random;
+using UnityEngine.UI;
+using System.Collections;
+
+/// http://www.mikedoesweb.com/2012/camera-shake-in-unity/
+
+public class AfterPressed : MonoBehaviour {
+
+	public GameObject text;
+	private IEnumerator coroutine;
+
+	private void OnEnable()
+	{
+		GetComponent<LongPressGesture>().LongPressed += longPressedHandler;
+	}
+
+	private void OnDisable()
+	{
+		GetComponent<LongPressGesture>().LongPressed -= longPressedHandler;
+	}
+
+	private void longPressedHandler(object sender, EventArgs e)
+	{
+		print ("adfasdfasdfasdfadf");
+		text.SetActive (true);
+		coroutine = changeAlpha(5.0f);
+		StartCoroutine(coroutine);
+	}
+
+	private IEnumerator changeAlpha(float waitTime)
+	{
+		Color color = GetComponent<Text> ().color;
+		color.a = 255f;
+		while (true)
+		{
+			GetComponent<Text> ().color = color;
+			yield return new WaitForSeconds(waitTime);
+			color.a = 0f;
+			GetComponent<Text> ().color = color;
+		}
+	}
+}
