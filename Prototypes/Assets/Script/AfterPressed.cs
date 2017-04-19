c950e576a8f054cfaff2e503d83abbf793613157
sxl <shenxia3@mail.utoronto.ca>
2017-04-18 22:49:07 -0700

    Tap to show text

--- Prototypes/Assets/Script/AfterPressed.cs
+++ Prototypes/Assets/Script/AfterPressed.cs
@@ -1,45 +1,39 @@
 ﻿using System;
 using UnityEngine;
-using TouchScript.Gestures;
+using UnityEngine.EventSystems;
 using Random = UnityEngine.Random;
 using UnityEngine.UI;
 using System.Collections;
 
 /// http://www.mikedoesweb.com/2012/camera-shake-in-unity/
 
-public class AfterPressed : MonoBehaviour {
+public class AfterPressed : MonoBehaviour, IPointerClickHandler {
 
 	public GameObject text;
 	private IEnumerator coroutine;
 
-	private void OnEnable()
-	{
-		GetComponent<LongPressGesture>().LongPressed += longPressedHandler;
+	public void OnPointerClick(PointerEventData eventData) {
+		clicked();
 	}
 
-	private void OnDisable()
-	{
-		GetComponent<LongPressGesture>().LongPressed -= longPressedHandler;
-	}
-
-	private void longPressedHandler(object sender, EventArgs e)
+	private void clicked()
 	{
 		print ("adfasdfasdfasdfadf");
 		text.SetActive (true);
-		coroutine = changeAlpha(5.0f);
+		coroutine = changeAlpha(3.0f);
 		StartCoroutine(coroutine);
 	}
 
 	private IEnumerator changeAlpha(float waitTime)
 	{
-		Color color = GetComponent<Text> ().color;
+		Color color = text.GetComponent<Text>().color;
 		color.a = 255f;
 		while (true)
 		{
-			GetComponent<Text> ().color = color;
+			text.GetComponent<Text>().color = color;
 			yield return new WaitForSeconds(waitTime);
 			color.a = 0f;
-			GetComponent<Text> ().color = color;
+			text.GetComponent<Text>().color = color;
 		}
 	}
 }
