409d60f417fb48d8dde2537447d0b0ff46c367e2
sxl <shenxia3@mail.utoronto.ca>
2017-09-10 14:20:35 -0700

    Movie Texture & Git Ignore

---
+++ Prototypes/Assets/MovieScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(AudioSource))]
+
+public class MovieScript : MonoBehaviour {
+
+
+	public MovieTexture movieclips;
+
+	private MeshRenderer meshRenderer;
+
+
+	void Start(){
+		meshRenderer = GetComponent<MeshRenderer>();
+		meshRenderer.material.mainTexture = movieclips;
+
+		AudioSource audio = GetComponent<AudioSource>();
+		audio.Play();
+
+		movieclips.Play ();
+		movieclips.loop = true;
+	}
+
+}
